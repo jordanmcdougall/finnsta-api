666b7c8d3557bd55d057006583ff3d7446f1e90e
jmcdougall <jordan.mcdougall@dxc.com>
2020-12-04 18:24:23 +0000

    feat: add privacy page

---
+++ pages/privacy.vue
@@ -0,0 +1,6 @@
+<template>
+  <v-container>
+    <h1>Privacy</h1>
+    <p>We collect app useage data in order to provide app functionality as well as to understand usage data</p>
+  </v-container>
+</template>
